caed9242554c40a1817efc73b0339589517a6e62
leeejunu <leeejunu@gmail.com>
2025-08-05 22:09:41 +0900

    spring

---
+++ src/main/java/com/spring/ExRateData.java
@@ -0,0 +1,11 @@
+package com.spring;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRateData(String result, Map<String, BigDecimal> rates) {
+}
